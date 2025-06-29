9f21de0c7d6cca9b343a9c97bb19084afa76c873
Ongeri123 <ongerinewton10@gmail.com>
2025-06-29 22:06:33 +0300

    using fetch and catch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const idElement = document.createElement("div");
+      idElement.textContent = data.id;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement("div");
+      errorElement.textContent = error.message;
+      document.body.appendChild(errorElement);
+    });
+}
